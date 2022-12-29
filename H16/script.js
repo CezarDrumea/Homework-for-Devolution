cf3949611366b298ccfafb4af8d41f8cc84b3e82
Cezar <drumea.cezar@gmail.com>
2022-12-29 02:41:35 +0200

    feat: Finished homework 16

---
+++ H16/script.js
@@ -0,0 +1,16 @@
+let userNationality = "Moldovan";
+let userName = "Jora";
+let user = userName;
+console.log(user, "is", userName, "and he is", userNationality);
+let numberVariable = 15;
+let bigNumberVariable = 124012913251461341234132413n;
+let booleanVariable = true;
+let nullVariable = null;
+let undefinedVariable = undefined;
+let stringVariable = "JavaScript is cool";
+let objectVariable = {
+  year: 2022,
+  month: "December",
+  day: 27,
+}
+console.log(numberVariable + "\n" + bigNumberVariable + "\n" + booleanVariable + "\n" + nullVariable + "\n" + undefinedVariable + "\n" + stringVariable + "\n" + "today's date is:", objectVariable.day, objectVariable.month, objectVariable.year);
